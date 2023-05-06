409cf17aab5fb48851f5d233afce72ba6ed20357
franco-giglio <giglio4605@gmail.com>
2023-05-06 12:05:53 -0300

    Agregar archivos de proyecto.

---
+++ MvcEmployee/Migrations/20230506144236_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcEmployee.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Departamento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salario = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ MvcEmployee/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcEmployee.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Nombre  { get; set; }
+    public int Edad { get; set; }
+    public string? Departamento { get; set; }
+    public decimal Salario{ get; set; }
+}
